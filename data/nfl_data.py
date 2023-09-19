208db32df9554ac0f1f6b000c84ef14778eaa114
Nick Hofstetter <nickhofstetter99@gmail.com>
2023-09-19 22:15:01 +0200

    added season list and team abbreviations list

--- data/nfl_data.py
+++ data/nfl_data.py
@@ -4,6 +4,21 @@ file.
 The CSV file will be used to analyze the data without having to do a request on the website everytime.
 """
 
+# random and time is used to follow the rules from the website
 import numpy as np
 import pandas as pd
+import random
+import time
 
+# list of seasons
+seasons = [str(season) for season in range(2010,2023)]
+
+# list of team abbreviations
+team_abbrs = ['crd', 'atl', 'rav', 'buf',
+              'car', 'chi', 'cin', 'cle',
+              'dal', 'den', 'det', 'gnb',
+              'htx', 'clt', 'jax', 'kan',
+              'sdg', 'ram', 'rai', 'mia',
+              'min', 'nwe', 'nor', 'nyg',
+              'nyj', 'phi', 'pit', 'sea',
+              'sfo', 'tam', 'oti', 'was']
