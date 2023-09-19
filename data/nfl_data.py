80d775901507737ba005b560532ea4606d1cdffc
Nick Hofstetter <nickhofstetter99@gmail.com>
2023-09-19 22:04:17 +0200

    file to get the boxscore of nfl games and the basic stats

---
+++ data/nfl_data.py
@@ -0,0 +1,9 @@
+"""
+In this python file I want to get Data from the Pro-Football-Reference site and export it into a CSV
+file.
+The CSV file will be used to analyze the data without having to do a request on the website everytime.
+"""
+
+import numpy as np
+import pandas as pd
+
