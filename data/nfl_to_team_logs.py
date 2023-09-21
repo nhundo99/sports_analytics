58a2379c0eba26075fb728b9820f3edcb356018e
Nick Hofstetter <nickhofstetter99@gmail.com>
2023-09-21 23:10:45 +0200

    completed file to convert the entire nfl gamelogs to each teams gamelog

--- data/nfl_to_team_logs.py
+++ data/nfl_to_team_logs.py
@@ -7,3 +7,34 @@ import numpy as np
 import pandas as pd
 
 
+# first we have to convert the csv file to a DataFrame
+nfl_df = pd.read_csv('nfl_gamelogs_2010_2023.csv')
+print(nfl_df)
+
+# find out what types are used in the dataframe 
+print(nfl_df.dtypes)
+
+# list of team abbreviations
+team_abbrs = ['crd', 'atl', 'rav', 'buf',
+              'car', 'chi', 'cin', 'cle',
+              'dal', 'den', 'det', 'gnb',
+              'htx', 'clt', 'jax', 'kan',
+              'sdg', 'ram', 'rai', 'mia',
+              'min', 'nwe', 'nor', 'nyg',
+              'nyj', 'phi', 'pit', 'sea',
+              'sfo', 'tam', 'oti', 'was']
+
+# iterate through all the teams
+for team in team_abbrs:
+    # make team dataframe
+    team_df = nfl_df[nfl_df['Team'] == team.upper()]
+
+    # save the team dataframe as a csv file in the given folder
+    team_df.to_csv('teams/' + team + '/' + team + '_2010_2023_gamelogs')
+
+
+"""
+# dataframe for crd
+crd_df = nfl_df[nfl_df['Team'] == 'CRD']
+print(crd_df)
+"""
