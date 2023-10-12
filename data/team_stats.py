dbeed3ebd5bd48221ac9dcb134149dffe09bbeb7
Nick Hofstetter <nickhofstetter99@gmail.com>
2023-10-12 22:44:46 +0200

    deleted team_stats.py since i created 2 files for the same purpose

--- data/team_stats.py
+++
@@ -1,8 +0,0 @@
-"""
-in this file i want to extract some team stats for each season
-we could probably also get these stats from pro-football-reference.com
-"""
-
-import numpy as np
-import pandas as pd
-
